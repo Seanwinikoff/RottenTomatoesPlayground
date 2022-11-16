a3772f30408a4ab94fd5327e0da998db74533616
Sean Winikoff <104329741+Seanwinikoff@users.noreply.github.com>
2022-11-16 15:41:55 -0500

    Update RottenTomato Scripts.sql

--- RottenTomato Scripts.sql
+++ RottenTomato Scripts.sql
@@ -1,5 +1,4 @@
 --this selects the top movies/decade
-
 select count(1),(date_part('decade', release_date) * 10) as decade
 from movies
 group by decade
@@ -8,7 +7,6 @@ order by decade desc;
 ----------------------------------------------------------------------------------------------------------
 
 --this shows how many times these actors show up in movies-- uses joins to connect all 3 tables together 
-
 select count(entertainment_workers.name), entertainment_workers."name" names
 from  movies  
 inner join movie_workers  
